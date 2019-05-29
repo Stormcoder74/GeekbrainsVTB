7903e6ebc6f23894b87082bdbd7b1aab4d8b7b18
Stormcoder <teryaevs@yandex.ru>
2019-05-30 02:13:39 +1100

    ДЗ 6 версия 2 работает с любами размерами и количеством потоков

---
+++ less06/less06_dz_v2/src/main/java/com/geekbrains/teryaevs/MainApp.java
@@ -0,0 +1,84 @@
+package com.geekbrains.teryaevs;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainApp {
+    private static final int NUM_THREADS = 3451;
+    private static final int SIZE = 34_654_737;
+    private static final int PART = SIZE / NUM_THREADS;
+
+    static class MyThread implements Runnable {
+        float[] arr;
+        int offset;
+
+        MyThread(float[] arr, int offset) {
+            this.arr = arr;
+            this.offset = offset;
+        }
+
+        @Override
+        public void run() {
+            int end = Math.min(offset + PART, SIZE);
+            for (int i = offset; i < end; i++) {
+                arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) *
+                        Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        }
+    }
+
+    private static float[] method1() {
+        float[] array = new float[SIZE];
+        Arrays.fill(array, 1.0f);
+
+        long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < SIZE; i++)
+            array[i] = (float) (array[i] * Math.sin(0.2f + i / 5) *
+                    Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+
+        System.out.println("method1(): " + (System.currentTimeMillis() - startTime) + " мсек.");
+        return array;
+    }
+
+    private static float[] method2() {
+        float[] array = new float[SIZE];
+        Arrays.fill(array, 1.0f);
+
+        long startTime = System.currentTimeMillis();
+
+        ArrayList<Thread> threads = new ArrayList<>(NUM_THREADS);
+
+        for (int i = 0; i < NUM_THREADS + 1; i++) {
+            Thread thread = new Thread(new MyThread(array, PART * i));
+            thread.start();
+            threads.add(thread);
+        }
+
+        try {
+            for (Thread thread : threads) {
+                thread.join();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("method2(): " + (System.currentTimeMillis() - startTime) + " мсек.");
+        return array;
+    }
+
+    public static void main(String[] args) {
+        float[] arr1 = method1();
+        float[] arr2 = method2();
+        for (int i = 0; i < SIZE; i++) {
+            if (Math.abs(arr1[i] - arr2[i]) > 0.0001) {
+                System.out.println("массивы не эквивалентны " + i);
+                return;
+            }
+        }
+        System.out.println("массивы эквивалентны");
+    }
+}
+
+//    method1(): 47201 мсек.
+//    method2(): 7722 мсек.
+//    массивы эквивалентны
