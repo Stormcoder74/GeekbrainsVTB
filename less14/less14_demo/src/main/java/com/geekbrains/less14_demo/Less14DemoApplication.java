e8e10a609d1c6df33894d13e6826471837497e83
Stormcoder <teryaevs@yandex.ru>
2019-06-12 13:17:37 +1100

    сгенерированный Spring Initializr проект для ДЗ 14

---
+++ less14/less14_demo/src/main/java/com/geekbrains/less14_demo/Less14DemoApplication.java
@@ -0,0 +1,13 @@
+package com.geekbrains.less14_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Less14DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Less14DemoApplication.class, args);
+	}
+
+}
