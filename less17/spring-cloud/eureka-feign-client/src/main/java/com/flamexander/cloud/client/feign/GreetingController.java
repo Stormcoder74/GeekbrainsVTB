185070e0620bc70c04c816977b04686c664e562c
Stormcoder <teryaevs@yandex.ru>
2019-06-19 14:27:49 +1100

    работает просмотр и форма редактирования/добавления

--- less17/spring-cloud/eureka-feign-client/src/main/java/com/flamexander/cloud/client/feign/GreetingController.java
+++ less17/spring-cloud/eureka-feign-client/src/main/java/com/flamexander/cloud/client/feign/GreetingController.java
@@ -3,12 +3,12 @@ package com.flamexander.cloud.client.feign;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/product")
 public class GreetingController {
     private GreetingClient greetingClient;
 
@@ -17,17 +17,29 @@ public class GreetingController {
         this.greetingClient = greetingClient;
     }
 
-    @GetMapping("/product/{id}")
+    @GetMapping("/{id}")
     public String showProduct(Model model, @PathVariable Long id) {
         Product product = greetingClient.greeting(id);
         model.addAttribute("product", product);
         return "show-product";
     }
 
-    @GetMapping("/product")
+    @GetMapping
     public String showProductsList(Model model) {
         List<Product> products = greetingClient.getProducts();
         model.addAttribute("products", products);
         return "products";
+    }
+
+    @GetMapping("/add_page")
+    public String showAddPage(Model model) {
+        model.addAttribute("product", new Product());
+        return "product-page";
+    }
+
+    @GetMapping("/edit_page")
+    public String showEditPage(Model model, @ModelAttribute(value = "product") Product product) {
+        model.addAttribute("product", product);
+        return "product-page";
     }
 }
